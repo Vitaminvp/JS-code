0fafdd5062a3e5deb99ff1858830478ef389b73e
Vitamin <vitamin@ukr.net>
2018-06-12 17:55:26 +0300

    finish

--- 04 - Array Cardio Day 1/js/script.js
+++
@@ -1,9 +0,0 @@
-var inputs = document.querySelectorAll(".controls input");
-
-inputs.forEach(input => input.addEventListener("change", changeValue));
-inputs.forEach(input => input.addEventListener("mousemove", changeValue));
-
-function changeValue(e){
-    var suffix = this.dataset.sizing || "";
-    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-}
