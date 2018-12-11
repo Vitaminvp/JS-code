aaf60089e046ff410a8f07a7aa25085b46f9f45e
Vitamin <vitamin@ukr.net>
2018-12-11 17:50:49 +0200

    finish

--- Complete JavaScript/Forkify/src/js/index.js
+++ Complete JavaScript/Forkify/src/js/index.js
@@ -2,8 +2,21 @@ import Search from './models/Search';
 
 const state = {};
 
-const search = new Search('pizza');
+const controlSearch = async () => {
+    const query = 'pizza';
+    if(query){
+        state.search = new Search(query);
+        await state.search.getResults();
+        console.log("state.search.result", state.search.recipes);
+    }
+};
 
-search.getResults();
+const btn = document.querySelector('.search');
+btn.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+// search.getResults();
 
 
--- Complete JavaScript/Forkify/src/js/models/Search.js
+++ Complete JavaScript/Forkify/src/js/models/Search.js
@@ -5,11 +5,10 @@ export default class Search{
     constructor(query){
         this.query = query;
     }
-    async getResults(query) {
+    async getResults() {
         try{
             const result = await axios(`${URL}?key=${KEY1}&q=${this.query}`);
             this.recipes = result.data.recipes;
-            console.log("recipes", this.recipes);
         } catch(error) {
             console.error(error);
         }
