fd2245088fde006ab9a73f7754d2c5f110448eb9
Vitamin <vitamin@ukr.net>
2018-12-13 18:04:15 +0200

    finish

--- codewars/src/12.js
+++ codewars/src/12.js
@@ -7,7 +7,21 @@
 
 const checkNum = (str) => {
     const s = String(str);
-    return s.match(/[\d|.\+]+/g).join(', ');
+    // return s.match(/[\d|.\+]+/g).join(', ');
+    let arr = ["3/4 cups Fresh Basil Leaves",
+    "1 1/2 cup Grated Parmesan Cheese",
+    "3 Tablespoons Pine Nuts",
+    "2 cloves Garlic, Peeled",
+    "Salt And Pepper, to taste",
+    "1/3 cup Extra Virgin Olive Oil",
+    "1-1/2 cup Heavy Cream",
+    "2 Tablespoons Butter",
+    "1/4 cup Grated Parmesan (additional)",
+    "12 ounces, weight Pasta (cavitappi, Fusili, Etc.)",
+    "2 whole Tomatoes, Diced"]
+    return arr.map(item => ({
+        num: item.match(/^[\d|/|-| \+]+/g) ? item.match(/^[\d|/|-| \+]+/g)[0].trim() : '',
+    }));
 };
 
-console.log("", checkNum('234sd23424werwe446er7.4sdh456 89 88  .666'));
+console.log("", checkNum('2 3/4 sd23424werwe446er7.4sdh456 89 88  .666'));
