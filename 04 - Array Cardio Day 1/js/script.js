632f237a466e876142f7b7636b5cd2937aaa096f
Vitalii Ovcharenko <vitamin@ukr.net>
2019-01-10 10:06:39 +0200

    Add readme

--- 04 - Array Cardio Day 1/js/script.js
+++
@@ -1,8 +0,0 @@
-// const box = document.querySelector('.box');
-// console.log("hhh");
-// box.addEventListener('click', function () {
-//
-//     console.log(this);
-//     this.classList.toggle('opening');
-//
-// });
